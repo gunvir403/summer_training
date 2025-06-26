e269e36ec01a3799fab604895f7950613b8c2422
gunvir403 <gunvir404@gmail.com>
2025-06-26 16:19:27 +0530

    modified:   day_13/weather_app/index.html 	modified:   day_13/weather_app/script.js 	modified:   day_13/weather_app/styles.css

--- day_13/weather_app/script.js
+++ day_13/weather_app/script.js
@@ -4,7 +4,14 @@ const cityInput = document.getElementById("inputBar");
 const weatherInfo = document.getElementById("weatherInfo");
 const forecastInfo = document.getElementById("forecastInfo");
 const errorMessage = document.getElementById("errorMessage");
+const recentList = document.getElementById("recents");
 searchBtn.addEventListener("click", getWeather);
+
+cityInput.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        searchBtn.click();
+    }
+});
 async function getWeather() {
     const city = cityInput.value.trim();
     if (!city) {
@@ -18,7 +25,6 @@ async function getWeather() {
         const data = await response.json();
         const lat = data.coord.lat;
         const lon = data.coord.lon;
-
         const url2 =`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
         const response2 = await fetch(url2);
         const forecastData = await response2.json();
@@ -31,29 +37,65 @@ async function getWeather() {
         displayForecast(forecastData);
     }
   } catch (error) {
-    console.log(error)
-    errorMessage.textContent = "Error fetching weather data. Please try again.";
-    weatherInfo.innerHTML = "";
+        console.log(error);
+        errorMessage.textContent = "Error fetching weather data. Please try again.";
+        weatherInfo.innerHTML = "";
+        forecastInfo.innerHTML = "";
   }
 }
 function displayWeather(data) {
-    weatherInfo.style.display = "block";
-    const temp = data.main.temp;
-    const humidity = data.main.humidity;
-    const windSpeed = data.wind.speed;
-    weatherInfo.innerHTML =`<div class="details">
-                                <p>Temperature: ${temp}°C</p>
-                                <p>Humidity: ${humidity}%</p>
-                                <p>Wind Speed: ${windSpeed} m/s</p>
-                            </div>`;
+    weatherInfo.style.display = "flex";
+    weatherInfo.innerHTML =`    <h2><i class="fa-solid fa-location-dot"></i>${data.name}</h2>
+                                <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
+                                <h1>${Math.trunc(data.main.temp)}°C<p>${data.weather[0].description}</p></h1>
+                                <div class="details"><p>Humidity<br><br>${data.main.humidity}%</p></div>
+                                <div class="details"><p>Wind Speed<br><br>${data.wind.speed} m/s</p></div>
+                                <h2>5-Day Forecast</h2>`;
 }
 function displayForecast(forecastData) {
-    forecastInfo.style.display = "block";
+    forecastInfo.innerHTML = "";
+    forecastInfo.style.display = "flex";
     for(let i = 0; i < 5; i++) {
         const temp2 = forecastData.list[i].main.temp;
-        const forecast = document.createElement('p');
-        forecast.textContent = `Day ${i+1} :` + temp2;
+        const forecast = document.createElement('div');
+        forecast.classList.add("details");
+        forecast.innerHTML = `Day ${i+1}<br><img src="https://openweathermap.org/img/wn/${forecastData.list[i].weather[0].icon}@2x.png"><br>${Math.trunc(forecastData.list[i].main.temp)}°C<br>${forecastData.list[i].weather[0].description}`;
         forecastInfo.appendChild(forecast);
-        
     }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    function loadRecents() {
+        const recents = JSON.parse(localStorage.getItem("recents")) || [];
+        recents.forEach(recentText => addRecentsToDOM(recentText));
+    }
+    function saveRecents() {
+        const recents = [];
+        document.querySelectorAll("#recents p").forEach(p => {
+            recents.push(p.textContent);
+        });
+        localStorage.setItem("recents", JSON.stringify(recents));
+    }
+
+    function addRecentsToDOM(recentText) {
+        const recentPara = document.createElement('p');
+        recentPara.textContent = recentText;
+        recentList.appendChild(recentPara);
+    }
+
+    searchBtn.addEventListener("click", () => {
+        const recentText = cityInput.value.trim();
+        if (recentText !== "") {
+            recentList.style.display = "flex";
+            addRecentsToDOM(recentText);
+            saveRecents();
+            cityInput.value = "";
+        }
+    });
+    cityInput.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+            searchBtn.click()
+        } 
+    })
+    loadRecents();
+});
